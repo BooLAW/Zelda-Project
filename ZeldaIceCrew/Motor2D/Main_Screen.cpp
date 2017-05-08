1c877bcbf27f7ecce2b3ce7d1b5fb51a86c3f6e6
rogerbusquets97 <duranbusquetsroger@gmail.com>
2017-05-08 21:02:45 +0200

    Controls menu
    Controls menu added. Now we can navigate through all the menu, but we
    must code whatever we want the program to do when an option is clicked.

--- ZeldaIceCrew/Motor2D/Main_Screen.cpp
+++ ZeldaIceCrew/Motor2D/Main_Screen.cpp
@@ -132,6 +132,163 @@ bool Main_Screen::Start()
 	back->movable = true;
 	back->pos = { Back->pos.x + 550, Back->pos.y + 2 };
 
+	///////////////////////////
+
+	Up = (GuiImage*)App->gui->CreateElement(image);
+	Up->pos = {250,100};
+	Up->texture_rect = { 0, 514, 683, 36 };
+	Up->active = false;
+	Up->movable = true;
+	controls_elements.push_back(Up);
+
+	up = (GuiText*)App->gui->CreateElement(text);
+	up->str = "Up";
+	up->active = false;
+	up->movable = true;
+	up->pos = { Up->pos.x + 550, Up->pos.y + 2 };
+
+	Down = (GuiImage*)App->gui->CreateElement(image);
+	Down->pos = { Up->pos.x,Up->pos.y+50 };
+	Down->texture_rect = { 0, 514, 683, 36 };
+	Down->active = false;
+	Down->movable = true;
+	controls_elements.push_back(Down);
+
+	down = (GuiText*)App->gui->CreateElement(text);
+	down->str = "Down";
+	down->active = false;
+	down->movable = true;
+	down->pos = { Down->pos.x + 550, Down->pos.y + 2 };
+
+	Right = (GuiImage*)App->gui->CreateElement(image);
+	Right->pos = { Down->pos.x,Down->pos.y + 50 };
+	Right->texture_rect = { 0, 514, 683, 36 };
+	Right->active = false;
+	Right->movable = true;
+	controls_elements.push_back(Right);
+
+	right = (GuiText*)App->gui->CreateElement(text);
+	right->str = "Right";
+	right->active = false;
+	right->movable = true;
+	right->pos = {Right->pos.x + 550, Right->pos.y + 2 };
+
+	Left = (GuiImage*)App->gui->CreateElement(image);
+	Left->pos = { Right->pos.x,Right->pos.y + 50 };
+	Left->texture_rect = { 0, 514, 683, 36 };
+	Left->active = false;
+	Left->movable = true;
+	controls_elements.push_back(Left);
+
+	left = (GuiText*)App->gui->CreateElement(text);
+	left->str = "Left";
+	left->active = false;
+	left->movable = true;
+	left->pos = { Left->pos.x + 550, Left->pos.y + 2 };
+
+	Move_up = (GuiImage*)App->gui->CreateElement(image);
+	Move_up->pos = { Left->pos.x,Left->pos.y + 50 };
+	Move_up->texture_rect = { 0, 514, 683, 36 };
+	Move_up->active = false;
+	Move_up->movable = true;
+	controls_elements.push_back(Move_up);
+
+	move_up = (GuiText*)App->gui->CreateElement(text);
+	move_up->str = "Move Up";
+	move_up->active = false;
+	move_up->movable = true;
+	move_up->pos = { Move_up->pos.x + 550, Move_up->pos.y + 2 };
+
+	Move_down = (GuiImage*)App->gui->CreateElement(image);
+	Move_down->pos = { Move_up->pos.x,Move_up->pos.y + 50 };
+	Move_down->texture_rect = { 0, 514, 683, 36 };
+	Move_down->active = false;
+	Move_down->movable = true;
+	controls_elements.push_back(Move_down);
+
+	move_down = (GuiText*)App->gui->CreateElement(text);
+	move_down->str = "Move Down";
+	move_down->active = false;
+	move_down->movable = true;
+	move_down->pos = { Move_down->pos.x + 550, Move_down->pos.y + 2 };
+
+	Move_right = (GuiImage*)App->gui->CreateElement(image);
+	Move_right->pos = { Move_down->pos.x,Move_down->pos.y + 50 };
+	Move_right->texture_rect = { 0, 514, 683, 36 };
+	Move_right->active = false;
+	Move_right->movable = true;
+	controls_elements.push_back(Move_right);
+
+	move_right = (GuiText*)App->gui->CreateElement(text);
+	move_right->str = "Move Right";
+	move_right->active = false;
+	move_right->movable = true;
+	move_right->pos = { Move_right->pos.x + 550, Move_right->pos.y + 2 };
+
+	Move_left = (GuiImage*)App->gui->CreateElement(image);
+	Move_left->pos = {Move_right->pos.x,Move_right->pos.y + 50 };
+	Move_left->texture_rect = { 0, 514, 683, 36 };
+	Move_left->active = false;
+	Move_left->movable = true;
+	controls_elements.push_back(Move_left);
+
+	move_left = (GuiText*)App->gui->CreateElement(text);
+	move_left->str = "Move_left";
+	move_left->active = false;
+	move_left->movable = true;
+	move_left->pos = { Move_left->pos.x + 550, Move_left->pos.y + 2 };
+
+	Menu = (GuiImage*)App->gui->CreateElement(image);
+	Menu->pos = { Move_left->pos.x,Move_left->pos.y + 50 };
+	Menu->texture_rect = { 0, 514, 683, 36 };
+	Menu->active = false;
+	Menu->movable = true;
+	controls_elements.push_back(Menu);
+
+	menu = (GuiText*)App->gui->CreateElement(text);
+	menu->str = "Menu";
+	menu->active = false;
+	menu->movable = true;
+	menu->pos = { Menu->pos.x + 550, Menu->pos.y + 2 };
+
+	Action = (GuiImage*)App->gui->CreateElement(image);
+	Action->pos = { Menu->pos.x,Menu->pos.y + 50 };
+	Action->texture_rect = { 0, 514, 683, 36 };
+	Action->active = false;
+	Action->movable = true;
+	controls_elements.push_back(Action);
+
+	action = (GuiText*)App->gui->CreateElement(text);
+	action->str = "Action";
+	action->active = false;
+	action->movable = true;
+	action->pos = { Action->pos.x + 550, Action->pos.y + 2 };
+
+	Dash = (GuiImage*)App->gui->CreateElement(image);
+	Dash->pos = { Action->pos.x,Action->pos.y + 50 };
+	Dash->texture_rect = { 0, 514, 683, 36 };
+	Dash->active = false;
+	Dash->movable = true;
+	controls_elements.push_back(Dash);
+
+	dash = (GuiText*)App->gui->CreateElement(text);
+	dash->str = "Dash";
+	dash->active = false;
+	dash->movable = true;
+	dash->pos = { Dash->pos.x + 550, Dash->pos.y + 2 };
+
+	Back_controls = (GuiImage*)App->gui->CreateElement(image);
+	Back_controls->pos = { Dash->pos.x, Dash->pos.y + 50 };
+	Back_controls->texture_rect = { 0, 514, 683, 36 };
+	Back_controls->active = false;
+	Back_controls->movable = true;
+	controls_elements.push_back(Back_controls);
+
+	back_controls = (GuiText*)App->gui->CreateElement(text);
+	back_controls->str = "Back";
+	back_controls->active = false;
+	back_controls->movable = true;
+	back_controls->pos = { Back_controls->pos.x + 550, Back_controls->pos.y + 2 };
 
 	App->audio->PlayMusic("Audio/Music/Opening.ogg");
 
@@ -147,6 +304,24 @@ bool Main_Screen::Update(float dt)
 {
 	bool ret = true;
 	if (in_settings) {
+
+		for (std::list<UIElement*>::const_iterator it = controls_elements.cbegin(); it != controls_elements.cend(); it++) {
+			it._Ptr->_Myval->active = false;
+		}
+
+		up->active = false;
+		down->active = false;
+		right->active = false;
+		left->active = false;
+		move_up->active = false;
+		move_down->active = false;
+		move_right->active = false;
+		move_left->active = false;
+		menu->active = false;
+		action->active = false;
+		dash->active = false;
+		back_controls->active = false;
+////////////////////////////////////////////
 		New_game->active = false;
 		new_game->active = false;
 		Continue->active = false;
@@ -184,7 +359,9 @@ bool Main_Screen::Update(float dt)
 		if (App->input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN) {
 			App->audio->PlayFx(press_fx);
 			if (selected == Controls) {
-				// go to change controls scene
+				in_settings = false;
+				in_controls = true;
+				selected = controls_elements.front();
 				
 			}
 			if (selected == Mute) {
@@ -194,13 +371,137 @@ bool Main_Screen::Update(float dt)
 
 				selected = ui_elements.front();
 				in_settings = false;
+				in_controls = false;
 			}
 		
 		}
 
 
 	}
+	else if (in_controls) {
+
+		New_game->active = false;
+		new_game->active = false;
+		Continue->active = false;
+		cont->active = false;
+		Settings->active = false;
+		settings->active = false;
+		Exit->active = false;
+		exit->active = false;
+
+		Controls->active = false;
+		controls->active = false;
+		Mute->active = false;
+		mute->active = false;
+		Back->active = false;
+		back->active = false;
+
+		for (std::list<UIElement*>::const_iterator it = controls_elements.cbegin(); it != controls_elements.cend(); it++) {
+			it._Ptr->_Myval->active = true;
+		}
+
+		up->active = true;
+		down->active = true;
+		right->active = true;
+		left->active = true;
+		move_up->active = true;
+		move_down->active = true;
+		move_right->active = true;
+		move_left->active = true;
+		menu->active = true;
+		action->active = true;
+		dash->active = true;
+		back_controls->active = true;
+
+		for (std::list<UIElement*>::const_iterator it = controls_elements.cbegin(); it != controls_elements.cend(); it++) {
+			if (selected == it._Ptr->_Myval) {
+				it._Ptr->_Myval->texture_rect = { 0,473,683,36 };
+			}
+			else {
+				it._Ptr->_Myval->texture_rect = { 0, 514, 683, 36 };
+			}
+		}
+		if (App->input->GetKey(SDL_SCANCODE_UP) == KEY_DOWN) {
+			App->audio->PlayFx(fx);
+			selected = Controls_Prev();
+			/*if (!IsInsideCam(selected)) {
+				Scroll_Down();
+			}*/
+		}
+		if (App->input->GetKey(SDL_SCANCODE_DOWN) == KEY_DOWN) {
+			App->audio->PlayFx(fx);
+			selected = Controls_Next();
+			/*if (!IsInsideCam(selected)) {
+				Scroll_Up();
+			}*/
+
+		}
+		if (App->input->GetKey(SDL_SCANCODE_SPACE) == KEY_DOWN) {
+			App->audio->PlayFx(press_fx);
+			if (selected == Up) {
+				// go to change controls scene
+
+			}
+			if (selected == Down) {
+				//mute the app
+			}
+			if (selected == Right) {
+
+			}
+			if (selected == Left) {
+				//mute the app
+			}
+			if (selected == Move_left) {
+				//mute the app
+			}
+			if (selected == Move_up) {
+				//mute the app
+			}
+			if (selected == Move_down) {
+				//mute the app
+			}
+			if (selected == Move_right) {
+				//mute the app
+			}
+			if (selected == Menu) {
+				//mute the app
+			}
+			if (selected == Action) {
+				//mute the app
+			}
+			if (selected == Dash) {
+				//mute the app
+			}
+			if (selected == Back_controls) {
+				in_settings = true;
+				in_controls = false;
+				selected = settings_elements.front();
+			}
+
+
+		}
+		
+
+	}
 	else{
+
+		for (std::list<UIElement*>::const_iterator it = controls_elements.cbegin(); it != controls_elements.cend(); it++) {
+			it._Ptr->_Myval->active = false;
+		}
+
+		up->active = false;
+		down->active = false;
+		right->active = false;
+		left->active = false;
+		move_up->active = false;
+		move_down->active = false;
+		move_right->active = false;
+		move_left->active = false;
+		menu->active = false;
+		action->active = false;
+		dash->active = false;
+		back_controls->active = false;
+
 		New_game->active = true;
 		new_game->active = true;
 		Continue->active = true;
@@ -372,6 +673,87 @@ UIElement * Main_Screen::Settings_Prev()
 		}
 	}
 	
+}
+
+UIElement * Main_Screen::Controls_Prev()
+{
+	if (selected != nullptr) {
+		if (!controls_elements.empty()) {
+			if (selected == controls_elements.front()) {
+				return controls_elements.front();
+			}
+			for (std::list<UIElement*>::const_iterator it = controls_elements.cend(); it != controls_elements.cbegin(); it--) {
+				if (selected == it._Ptr->_Myval) {
+					if (selected == controls_elements.front()) {
+						return controls_elements.front();
+					}
+					else {
+						it--;
+						if (it._Ptr != nullptr)
+							return it._Ptr->_Myval;
+						else
+							return controls_elements.back();
+					}
+				}
+
+
+			}
+		}
+	}
+}
+
+UIElement * Main_Screen::Controls_Next()
+{
+	if (selected != nullptr) {
+		if (!controls_elements.empty()) {
+			if (selected == controls_elements.back()) {
+				return controls_elements.back();
+			}
+			else {
+				for (std::list<UIElement*>::const_iterator it = controls_elements.cbegin(); it != controls_elements.cend(); it++) {
+					if (selected == it._Ptr->_Myval) {
+						if (selected == controls_elements.back()) {
+							return controls_elements.back();
+						}
+						else {
+							it++;
+							if (it._Ptr != nullptr)
+								return it._Ptr->_Myval;
+							else
+								return controls_elements.back();
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+void Main_Screen::Scroll_Up()
+{
+	for (std::list<UIElement*>::const_iterator it = controls_elements.cbegin(); it != controls_elements.cend(); it++) {
+		it._Ptr->_Myval->pos.y -= 100;
+	}
+}
+
+void Main_Screen::Scroll_Down()
+{
+	for (std::list<UIElement*>::const_iterator it = controls_elements.cbegin(); it != controls_elements.cend(); it++) {
+		it._Ptr->_Myval->pos.y += 100;
+	}
+}
+
+bool Main_Screen::IsInsideCam(UIElement * element)
+{
+	if ((element->pos.x > App->render->camera.x) && (element->pos.x < App->render->camera.x + App->render->camera.w)) {
+		if ((element->pos.y > App->render->camera.y) && (element->pos.y < App->render->camera.y + App->render->camera.h)) {
+			return true;
+		}
+		else return false;
+	}
+	else {
+		return false;
+	}
 }
 
 bool Main_Screen::CleanUp()
--- ZeldaIceCrew/Motor2D/Main_Screen.h
+++ ZeldaIceCrew/Motor2D/Main_Screen.h
@@ -37,6 +37,12 @@ public:
 	UIElement* Prev();
 	UIElement* Settings_Next();
 	UIElement* Settings_Prev();
+	UIElement* Controls_Prev();
+	UIElement* Controls_Next();
+	void Scroll_Up();
+	void Scroll_Down();
+
+	bool IsInsideCam(UIElement* element);
 
 	bool CleanUp();
 
@@ -44,6 +50,7 @@ private:
 
 	std::list<UIElement*> ui_elements;
 	std::list<UIElement*> settings_elements;
+	std::list<UIElement*> controls_elements;
 	UIElement* selected;
 	GuiImage* New_game;
 	GuiImage* Continue;
@@ -60,6 +67,7 @@ private:
 	///////
 
 	bool in_settings;
+	bool in_controls;
 
 	GuiImage* Controls;
 	GuiImage* Mute;
@@ -67,6 +75,33 @@ private:
 	GuiText* controls;
 	GuiText* mute;
 	GuiText* back;
+
+	////////////
+	GuiImage* Up;
+	GuiImage* Down;
+	GuiImage* Right;
+	GuiImage* Left;
+	GuiImage* Move_up;
+	GuiImage* Move_down;
+	GuiImage* Move_right;
+	GuiImage* Move_left;
+	GuiImage* Menu;
+	GuiImage* Action;
+	GuiImage* Dash;
+	GuiImage* Back_controls;
+
+	GuiText* up;
+	GuiText* down;
+	GuiText* right;
+	GuiText* left;
+	GuiText* move_up;
+	GuiText* move_down;
+	GuiText* move_right;
+	GuiText* move_left;
+	GuiText* menu;
+	GuiText* action;
+	GuiText* dash;
+	GuiText* back_controls;
 };
 
 #endif //__Main_Screen_H__
